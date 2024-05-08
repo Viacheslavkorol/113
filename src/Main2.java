773b23f63f5519f06f37d187e0454559e68dcb60
Viacheslavkorol <viacheslav.korol11@gmail.com>
2024-05-08 20:21:00 +0300

    first commit

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int a = 2;
+
+        while (2 * a - 1 < 5000) {
+            int result = 2 * a - 1;
+            System.out.print(result + " ");
+            a++;
+        }
+    }
+}
+
